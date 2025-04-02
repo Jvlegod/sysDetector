a78dd760cf51316166080311211fba4bd97915f1
Jvle <keke.oerv@isrc.iscas.ac.cn>
2025-04-02 23:15:19 +0800

    add interface for ebpf proc
    Signed-off-by: Jvle <keke.oerv@isrc.iscas.ac.cn>

--- src/sysDetector-server/src/ebpf.rs
+++ src/sysDetector-server/src/ebpf.rs
@@ -1,63 +1,50 @@
-use std::process::Command;
+use log::debug;
 use serde_json;
 use anyhow::Result;
-mod args;
+use crate::args::Command;
+use std::{path, process};
 
-pub fn handle_command(command: &[u8]) -> Result<String> {
-    let recv_command: Command = serde_json::from_slice(command)?;
-    println!("{:?}", recv_command);
+const EBPF_PATH: &str = "/home/jvle/Desktop/temp/";
+const PROC_NAME: &str = "proc";
 
-    let response = match recv_command {
-        Command::PROC { identifier, opt } => {
-            // execute proc
-            let output = Command::new("./ebpf_program")
-               .arg("PROC")
-               .arg(identifier)
-               .args(opt)
-               .output()?;
-
-            if output.status.success() {
-                let output_str = String::from_utf8_lossy(&output.stdout);
-                format!("Received PROC command. eBPF output: {}", output_str)
-            } else {
-                let error_str = String::from_utf8_lossy(&output.stderr);
-                format!("Error executing eBPF program for PROC: {}", error_str)
-            }
-        }
-        Command::FS { identifiers, opt } => {
-            // execute fs
-            let output = Command::new("./ebpf_program")
-               .arg("FS")
-               .args(identifiers)
-               .args(opt)
-               .output()?;
-
-            if output.status.success() {
-                let output_str = String::from_utf8_lossy(&output.stdout);
-                format!("Received FS command. eBPF output: {}", output_str)
-            } else {
-                let error_str = String::from_utf8_lossy(&output.stderr);
-                format!("Error executing eBPF program for FS: {}", error_str)
-            }
-        }
-        Command::LIST { identifiers, opt } => {
-            // execute list
-            let output = Command::new("./ebpf_program")
-               .arg("LIST")
-               .args(identifiers)
-               .args(opt)
-               .output()?;
-
-            if output.status.success() {
-                let output_str = String::from_utf8_lossy(&output.stdout);
-                format!("Received LIST command. eBPF output: {}", output_str)
-            } else {
-                let error_str = String::from_utf8_lossy(&output.stderr);
-                format!("Error executing eBPF program for LIST: {}", error_str)
-            }
-        }
-    };
-
-    Ok(response)
+pub struct Ebpf{
+    pub path: String,
 }
+
+impl Ebpf {
+    pub fn new() -> Result<Self> {
+        Ok(Self{
+            path: EBPF_PATH.to_string()
+        })  
+    }
+
+    pub fn handle_command(&self, command: &[u8]) -> Result<String> {
+        let recv_command: Command = serde_json::from_slice(command)?;
+        debug!("{:?}", recv_command);
     
+        let response = match recv_command {
+            Command::PROC { identifier, opt } => {
+                let full_path = format!("{}/{}", self.path, PROC_NAME);
+                let output = process::Command::new(full_path)
+                   .args(opt)
+                   .output()?;
+    
+                if output.status.success() {
+                    let output_str = String::from_utf8_lossy(&output.stdout);
+                    format!("Received PROC command. eBPF output: {}", output_str)
+                } else {
+                    let error_str = String::from_utf8_lossy(&output.stderr);
+                    format!("Error executing eBPF program for PROC: {}", error_str)
+                }
+            }
+            Command::FS { identifier, opt } => {
+                todo!()
+            }
+            Command::LIST { identifier, opt } => {
+                todo!()
+            }
+        };
+    
+        Ok(response)
+    }       
+}
--- src/sysDetector-server/src/main.rs
+++ src/sysDetector-server/src/main.rs
@@ -5,70 +5,32 @@ use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::UnixListener,
 };
-use anyhow::Result;
 
 mod ebpf;
 mod args;
 
-use self::{
-    args::{Command},
-};
+
+use anyhow::Result;
 
 // const SOCKET_FILE_NAME: &str = "/var/run/sysDetector.sock";
 const SOCKET_FILE_NAME: &str = "/home/jvle/Desktop/temp/sysDetector.sock";
 
-pub fn handle_command(command: &[u8]) -> Result<String> {
-    let recv_command: Command = serde_json::from_slice(command)?;
-    println!("{:?}", recv_command);
-    let response = match recv_command {
-        Command::PROC { identifier, opt } => {
-            format!("Received PROC command with identifier: {}, opt: {:?}", identifier, opt)
-        }
-        Command::FS { identifier, opt } => {
-            format!("Received FS command with identifiers: {:?}, opt: {:?}", identifier, opt)
-        }
-        Command::LIST { identifier, opt } => {
-            format!("Received LIST command with identifiers: {:?}, opt: {:?}", identifier, opt)
-        }
-    };
-    Ok(response)
-}
-
-async fn handle_connection(mut stream: tokio::net::UnixStream) {
-    // read length
+async fn handle_connection(mut stream: tokio::net::UnixStream) -> Result<()> {
+    let ebpf = ebpf::Ebpf::new()?;
     let mut len_buf = [0; 4];
-    if let Err(e) = stream.read_exact(&mut len_buf).await {
-        eprintln!("Error reading message length: {}", e);
-        return;
-    }
+    stream.read_exact(&mut len_buf).await?;
     let message_len = u32::from_be_bytes(len_buf) as usize;
 
-    // read content
     let mut buf = vec![0; message_len];
-    if let Err(e) = stream.read_exact(&mut buf).await {
-        eprintln!("Error reading message: {}", e);
-        return;
-    }
+    stream.read_exact(&mut buf).await?;
 
-    let response = match handle_command(&buf) {
-        Ok(resp) => resp,
-        Err(e) => {
-            eprintln!("Error handling command: {}", e);
-            "Error handling command".to_string()
-        }
-    };
+    let response = ebpf.handle_command(&buf)?;
 
-    // send length
     let response_len = response.len() as u32;
-    if let Err(e) = stream.write_all(&response_len.to_be_bytes()).await {
-        eprintln!("Error sending response length: {}", e);
-        return;
-    }
+    stream.write_all(&response_len.to_be_bytes()).await?;
+    stream.write_all(response.as_bytes()).await?;
 
-    // send content
-    if let Err(e) = stream.write_all(response.as_bytes()).await {
-        eprintln!("Error sending response: {}", e);
-    }
+    Ok(())
 }
 
 #[tokio::main]
